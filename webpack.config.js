0ee313acf815d5fa81a2e10fc5a7a57d4116c119
maxcowalenko <max.cowalenko2012@yandex.ru>
2021-04-30 16:37:48 +0300

    rename dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ const PATHS = {
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
   optimization: {
     splitChunks: {
       cacheGroups: {
